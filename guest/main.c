6a9f73139f0e220024d90808914cd63bbf2b3b1c
Jan Zwiener <jan@zwiener.org>
2025-11-01 01:25:07 +0100

    Simplified guest/main.c example

--- guest/main.c
+++ guest/main.c
@@ -16,22 +16,7 @@ static void print(const char *s)
 int main(void)
 {
     counter += initialized; // 42
-    print("Hello, World!\n");
-
-    float f = 1.0f;
-    for (int i = 0; i < 10; i++)
-    {
-        f += 0.5f;
-    }
-    if (f > 2.0f)
-    {
-        print("Float operations successful.\n");
-    }
-    else
-    {
-        print("Float operations failed.\n");
-    }
-    print("Counter value\n");
+    print("Hello, world!\n");
 
     return 0;
 }
