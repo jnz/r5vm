cc0b3faa5299451c45de8dc9040a768a62fbd2c8
Jan Zwiener <jan@zwiener.org>
2025-11-01 01:25:31 +0100

    Minor code cleanups for host application

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
+/*
+ * R5VM - Minimal RISC-V RV32I Virtual Machine
+ *       _____
+ *      | ____|
+ *  _ __| |____   ___ __ ___
+ * | '__|___ \ \ / / '_ ` _ \
+ * | |   ___) \ V /| | | | | |
+ * |_|  |____/ \_/ |_| |_| |_|
+ *
+ * Copyright (c) 2025 Jan Zwiener
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <string.h> // for strcmp
+#include <ctype.h> // for isspace, tolower
 
 #include "r5vm.h"
 
@@ -76,12 +106,12 @@ static bool load_file(const char* path, uint8_t** out_mem, size_t*
     fread(mem, 1, (size_t)fsize, f);
     fclose(f);
 
-	printf("[r5vm] program=%ld bytes (%ld.%02ld KiB), allocated=%zu bytes (%zu.%02zu KiB)%s\n",
-		fsize,
-		fsize / 1024, (fsize % 1024) * 100 / 1024,
-		total_mem,
-		total_mem / 1024, (total_mem % 1024) * 100 / 1024,
-		override_mem ? " (user override)" : "");
+    printf("[r5vm] program=%ld bytes (%ld.%02ld KiB), allocated=%zu bytes (%zu.%02zu KiB)%s\n",
+        fsize,
+        fsize / 1024, (fsize % 1024) * 100 / 1024,
+        total_mem,
+        total_mem / 1024, (total_mem % 1024) * 100 / 1024,
+        override_mem ? " (user override)" : "");
 
     *out_mem = mem;
     *out_mem_size = total_mem;
@@ -90,7 +120,7 @@ static bool load_file(const char* path, uint8_t** out_mem, size_t*
 
 // -------------------------------------------------------------
 
-void r5vm_dump_state(const r5vm_t* vm)
+static void r5vm_dump_state(const r5vm_t* vm)
 {
     if (!vm) return;
 
