0e35fbb53ef9c7dacd3ad887c76f99a1f374866e
Jan Zwiener <jan@zwiener.org>
2025-11-29 00:33:19 +0100

    Playground update

--- x86asmtest/test.s
+++ x86asmtest/test.s
@@ -7,17 +7,23 @@
 
 .global _main
 _main:
-    mov eax, [edi + 0xcc] # 0xcc = rs1
-    add eax, 0x1111111   # IMM_I (32-bit)
-    and eax, 0xFFFFFF # immediate and mask
-    add eax, [edi + 0xFF] # 0xFF = OFF_MEM
-    mov ebx, [edi + 0xdd] # 0xdd = rs2
-    mov [eax], ebx
+    # mov eax, [edi + 0xcc] # 0xcc = rs1
+    # add eax, 0x1111111   # IMM_I (32-bit)
+    # and eax, 0xFFFFFF # immediate and mask
+    # add eax, [edi + 0xFF] # 0xFF = OFF_MEM
+    # mov ebx, [edi + 0xdd] # 0xdd = rs2
+    # mov [eax], ebx
 
-    mov DWORD PTR [edi + 0x0c], 0xf0
-    mov DWORD PTR [edi + 0x0d], 0x0f
-    mov eax, [edi + 0x0c]
-    xor eax, [edi + 0x0d]
+    # mov DWORD PTR [edi + 0x0c], 0xf0
+    # mov DWORD PTR [edi + 0x0d], 0x0f
+
+    # mov eax, [edi + 0x0c]
+    # and eax, [edi + 0x0d]
+    # mov [edi + 0x0c], eax
+
+    mov ecx, [edi + 0x0c] # ebx = rs2
+    and ecx, 0x1f         # mask with 0x1f
+    shl eax, cl
 
     ret
 
